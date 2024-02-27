35a72493cb06579b63dc78c8adc79a7b7d6cf8fe
Clifton <yeoclifton@gmail.com>
2024-02-27 17:59:44 +0800

    Addresss code coverage tests

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -54,6 +54,7 @@ public class Person {
     public Address getAddress() {
         return address;
     }
+
     public Remark getRemark() {
         return remark;
     }
@@ -115,8 +116,8 @@ public class Person {
                 .add("phone", phone)
                 .add("email", email)
                 .add("address", address)
-                .add("tags", tags)
                 .add("remark", remark)
+                .add("tags", tags)
                 .toString();
     }
 
